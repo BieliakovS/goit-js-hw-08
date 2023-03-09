9e45126689b71f3323fc7f74a7126424e5c98832
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-03-10 01:12:03 +0200

    task 1,2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,14 +1,12 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
 
 import SimpleLightbox from 'simplelightbox';
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-
-const galleryEl = document.querySelector(".gallery");
+const galleryEl = document.querySelector('.gallery');
 const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
 
-galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsMarkup);
 
 function createGalleryItemsMarkup(gallery) {
   return gallery
@@ -19,15 +17,12 @@ function createGalleryItemsMarkup(gallery) {
     </a>
 `;
     })
-    .join("");
+    .join('');
 }
 
-let lightbox = new SimpleLightbox(".gallery .gallery__item", {
+let lightbox = new SimpleLightbox('.gallery .gallery__item', {
   captions: true,
-  captionsData: "alt",
+  captionsData: 'alt',
   captionDelay: 250,
+  elements: '.gallery__item',
 });
-
-// Change code below this line
-
-console.log(galleryItems);
